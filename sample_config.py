9a179a1fb3537a45cf718a02c1f0474e1f9a13e0
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-06-19 11:44:58 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -10,7 +10,7 @@ API_ID = int(environ.get("API_ID", "24089031"))
 API_HASH = environ.get("API_HASH", "0615e3afe13ddaaf8e9ddbd3977d35ff")
 SUDO_USERS_ID = [int(x) for x in environ.get("SUDO_USERS_ID", "5702598840 6584789596").split()]
 LOG_GROUP_ID = int(environ.get("LOG_GROUP_ID", "-1002100475470"))
-GBAN_LOG_GROUP_ID = int(environ.get("GBAN_LOG_GROUP_ID", ""-1002100475470))
+GBAN_LOG_GROUP_ID = int(environ.get("GBAN_LOG_GROUP_ID", "-1002100475470"))
 MESSAGE_DUMP_CHAT = int(environ.get("MESSAGE_DUMP_CHAT", "-1002100475470"))
 WELCOME_DELAY_KICK_SEC = int(environ.get("WELCOME_DELAY_KICK_SEC", "300"))
 MONGO_URL = environ.get("MONGO_URL", "mongodb+srv://Mongo1:586637515hshhwfftqu@cluster0.tvy79ai.mongodb.net/?retryWrites=true&w=majority")
