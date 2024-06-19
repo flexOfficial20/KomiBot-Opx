383c6484eac06beabdaf9508ecb01b74c415bb7c
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-06-19 11:47:12 +0530

    Update errors.py

--- Komi/core/decorators/errors.py
+++ Komi/core/decorators/errors.py
@@ -29,7 +29,7 @@ from functools import wraps
 
 from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
 
-from nezuko import LOG_GROUP_ID, app
+from komi import LOG_GROUP_ID, app
 
 
 def split_limits(text):
